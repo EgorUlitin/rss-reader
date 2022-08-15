1c2e7158fe694155fc1b7cb4d50335464c6dfc99
egorlitin <egorulitin@gmail.com>
2022-08-15 19:45:24 +0300

    fix linter error in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,36 +4,28 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-        // port: 8000,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-        }),
-    ],
-    module: {
-        rules: [
-          // {
-          //   test: /\.js$/,
-          //   exclude: /node_modules/,
-          //   use: {
-          //     loader: 'babel-loader',
-          //     options: {
-          //       presets: ['@babel/preset-env'],
-          //     },
-          //   },
-          // },
-          { test: /\.css$/, 
-            use: ['style-loader', 'css-loader'] },
-        ],
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+    // port: 8000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
+    ],
+  },
 };
